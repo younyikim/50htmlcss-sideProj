bc5d39ad01944312407beafc65a51b5f7542a1f2
younyikim <rlazmzm1123@gmail.com>
2021-09-28 11:48:51 +0900

    Progress Steps : JS 수정

--- 2.Progress_Steps/script.js
+++ 2.Progress_Steps/script.js
@@ -1,47 +1,51 @@
 const progress = document.querySelector(".progress");
-const steps = [...document.querySelectorAll(".step")];
+const steps = document.querySelectorAll(".step");
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 
-let totalStep = 4;
 let currentStep = 1;
-let percentage = [0, 33, 66, 99];
 
-function moveNext() {
-    currentStep++;
+function update() {
+    steps.forEach((step, index) => {
+        if (index < currentStep) {
+            step.classList.add("active");
+        } else {
+            step.classList.remove("active");
+        }
+    });
 
-    steps[currentStep - 1].classList.add("active");
+    const actives = document.querySelectorAll(".active");
 
-    progress.style.width = `${percentage[currentStep - 1]}%`;
+    progress.style.width =
+        ((actives.length - 1) / (steps.length - 1)) * 100 + "%";
 
-    // 현재 단계가 2단계 이상인 경우, prev 버튼 비활성화 해제
-    if (currentStep >= 2) {
-        prevBtn.removeAttribute("disabled");
-    }
-
-    // 현재 단계가 4단계인 경우, next 버튼 비활성화
-    if (currentStep >= 4) {
-        nextBtn.setAttribute("disabled", "");
-    }
-}
-
-function movePrev() {
-    currentStep--;
-
-    steps[currentStep].classList.remove("active");
-
-    progress.style.width = `${percentage[currentStep - 1]}%`;
-
-    // 현재 단계가 1단계일 때, prev 버튼을 비활성화
-    if (currentStep <= 1) {
-        prevBtn.setAttribute("disabled", "");
-    }
-    // 현재 단계가 3단계 이하일 경우, next 버튼 비활성화 제거
-    if (currentStep <= 3) {
-        nextBtn.removeAttribute("disabled");
+    if (currentStep == 1) {
+        prevBtn.disabled = true;
+    } else if (currentStep == steps.length) {
+        nextBtn.disabled = true;
+    } else {
+        prevBtn.disabled = false;
+        nextBtn.disabled = false;
     }
 }
 
 // Event Listener
-prevBtn.addEventListener("click", movePrev);
-nextBtn.addEventListener("click", moveNext);
+nextBtn.addEventListener("click", () => {
+    currentStep++;
+
+    if (currentStep > steps.length) {
+        currentStep = steps.length;
+    }
+
+    update();
+});
+
+prevBtn.addEventListener("click", () => {
+    currentStep--;
+
+    if (currentStep < 1) {
+        currentStep = 1;
+    }
+
+    update();
+});
